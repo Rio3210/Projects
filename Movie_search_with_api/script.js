e681c244b7b543c63b26d85a37905454194c283f
Surafel <surafelwork32@gmail.com>
2022-09-21 11:58:34 +0300

    Update script.js

--- Movie_search_with_api/script.js
+++ Movie_search_with_api/script.js
@@ -4,7 +4,7 @@ const resultGrid = document.getElementById('result-grid');
 
 
 async function loadMovies(searchTerm){
-    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=d6bba02d`;
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey={UseYourApiHere}`;
     const res = await fetch(`${URL}`);
     const data = await res.json();
     
@@ -52,7 +52,7 @@ function loadMovieDetails(){
         movie.addEventListener('click', async () => {
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=d6bba02d`);
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey={UseYourApiHere}`);
             const movieDetails = await result.json();
             displayMovieDetails(movieDetails);
         });
