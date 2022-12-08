1dd55717d6b6dd094354d03158781c88f94908e1
Surafel <surafelwork32@gmail.com>
2022-12-09 00:53:29 +0300

    Update settings.py
    second commit

--- python_blog/blog/blog/settings.py
+++ python_blog/blog/blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mi(jd(h4_an2swm5s*_zilg9+jm6!m6x(i4w_7i+%_bi9l#aq@'
+SECRET_KEY = 'provide your secret key here :))'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
