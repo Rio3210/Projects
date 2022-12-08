16f8a4ef8996bfc35d81b1e23749eff5a7163ca3
Surafel <surafelwork32@gmail.com>
2022-12-09 00:28:10 +0300

    Add files via upload
    Simple dynamic blog site using Django framework

---
+++ python_blog/blog/posts/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Post
+# Create your views here.
+def index(request):
+    posts=Post.objects.all()
+    return render(request,'index.html',{'posts':posts})
