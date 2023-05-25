239f5cd7c44e6e643210d29426f0d2a5627bb810
Surafel Workayehu <surafelwork32@gmail.com>
2023-05-25 23:13:16 +0300

    Delete python_blog/blog directory

--- python_blog/blog/posts/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-# Create your views here.
-def index(request):
-    posts=Post.objects.all()
-    return render(request,'index.html',{'posts':posts})
